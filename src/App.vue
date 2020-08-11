16bb1542cc8f141f502dbf0fe56b493142104f7b
Pete O'Shea <mail@peteoshea.co.uk>
2020-08-11 20:11:08 +0100

    If now categories setup then prompt for one first

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <template>
   <main>
-    <NavBar />
-    <div class="container flex">
-      <div class="w-1/2">
-        <BillsTable />
-      </div>
-      <div class="w-1/2">
-        <Chart :bills="activeBills" />
+    <AddCategory v-if="shouldShowAddCategory" v-on:addCategory="addCategory" />
+    <div v-else>
+      <NavBar />
+      <div class="container flex">
+        <div class="w-1/2">
+          <BillsTable />
+        </div>
+        <div class="w-1/2">
+          <Chart :bills="activeBills" />
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
-import Chart from "./components/Chart.vue";
+import AddCategory from "./components/AddCategory.vue";
 import BillsTable from "./components/BillsTable.vue";
+import Chart from "./components/Chart.vue";
+import NavBar from "./components/NavBar.vue";
 import "@/assets/css/tailwind.css";
 
 export default {
   name: "App",
   components: {
-    Chart,
+    AddCategory,
     BillsTable,
+    Chart,
     NavBar,
+  },
+  data() {
+    return {
+      bills: [],
+      categories: [],
+      shouldShowAddCategory: true,
+    };
+  },
+  methods: {
+    addCategory(category) {
+      this.categories.push(category);
+      this.shouldShowAddCategory = false;
+    },
   },
 };
 </script>
---
+++ src/components/AddCategory.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Enter a category of bills</h1>
+    <p>e.g. 'Electricity', 'Gas' or 'Internet'</p>
+    <input placeholder="Add category" v-model="category" />
+    <button @click="handleClick">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddCategory",
+  data: function () {
+    return {
+      category: "",
+    };
+  },
+  methods: {
+    handleClick: function () {
+      if (!this.category) {
+        alert("Enter a category");
+        return;
+      }
+      this.$emit("addCategory", this.category);
+    },
+  },
+};
+</script>
