3354026ae0077980b8c3850b84262f5f123d54f2
Pete O'Shea <mail@peteoshea.co.uk>
2020-08-11 20:15:42 +0100

    Style the add category form

--- src/components/AddCategory.vue
+++ src/components/AddCategory.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-    <h1>Enter a category of bills</h1>
-    <p>e.g. 'Electricity', 'Gas' or 'Internet'</p>
-    <input placeholder="Add category" v-model="category" />
-    <button @click="handleClick">Add</button>
+  <div class="h-100 w-full flex items-center justify-center font-sans">
+    <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+      <div class="mb-4">
+        <h1 class="text-gray-900">Enter a category of bills</h1>
+        <p>e.g. 'Electricity', 'Gas' or 'Internet'</p>
+        <div class="flex mt-4">
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-gray-900"
+            placeholder="Add category"
+            v-model="category"
+          />
+          <button
+            class="flex-no-shrink p-2 border-2 rounded bg-teal text-white border-teal hover:text-white hover:bg-teal"
+            @click="handleClick"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
